2daae0534f59a7613e8093bf98dae8a9b46f2857
Julian König <33655937+jkoenig134@users.noreply.github.com>
2023-01-26 16:29:37 +0100

    Fix/wording (#5)
    * chore: use bird backbone
    
    * fix: update keycloak server in compose file
    
    * chore: wording

--- packages/school-frontend/src/components/index.svelte
+++ packages/school-frontend/src/components/index.svelte
@@ -5,9 +5,9 @@
 <h3 class="w3-bar-item">Inhalt</h3>
 <ol>
   <li class="{index === 0 ? 'active' : ''} {index < 0 ? 'inactive' : ''} item">Zeugnis in ausgedruckter Form</li>
-  <li class="{index === 1 ? 'active' : ''} {index < 1 ? 'inactive' : ''} item">opt. download der App</li>
-  <li class="{index === 2 ? 'active' : ''} {index < 2 ? 'inactive' : ''} item">scannen des QR-Copdes</li>
-  <li class="{index === 4 ? 'active' : ''} {index < 3 ? 'inactive' : ''} item">betrachten der Ergebnisse</li>
+  <li class="{index === 1 ? 'active' : ''} {index < 1 ? 'inactive' : ''} item">Download der App</li>
+  <li class="{index === 2 ? 'active' : ''} {index < 2 ? 'inactive' : ''} item">Scannen des QR-Codes</li>
+  <li class="{index === 4 ? 'active' : ''} {index < 3 ? 'inactive' : ''} item">Betrachten der Ergebnisse</li>
 </ol>
 
 <style>
